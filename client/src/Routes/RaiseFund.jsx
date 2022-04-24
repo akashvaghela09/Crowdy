cc36fea4473830aef58ec49a693576335514c3b9
Akash Vaghela <akashvaghela09@gmail.com>
2022-04-25 00:25:33 +0530

    extra packages removed

--- client/src/Routes/RaiseFund.jsx
+++ client/src/Routes/RaiseFund.jsx
@@ -37,6 +37,7 @@ const RaiseFund = () => {
         httpOptions: { timeout: 0 },
     });
 
+    // upload Image to StorJ platform
     const uploadImage = async (e) => {
         let filename = `${Date.now()}-${file.name}`;
 
@@ -95,7 +96,7 @@ const RaiseFund = () => {
         let form = await toast.promise(
             contract.addForFunding(title, description, imageKey, address, amount),
             {
-                pending: 'Submitting contract...',
+                pending: 'Submitting ...',
                 success: 'Application Submitted 👌',
                 error: 'Something went wrong 🤯'
             }
@@ -117,9 +118,9 @@ const RaiseFund = () => {
     return (
         <div className='h-fit flex justify-center items-center flex-col'>
             <h1 className='text-3xl text-slate-800 font-bold p-10'>Fill Your Application</h1>
-            <div className='bg-slate-300 w-4/5 py-12 h-fit drop-shadow-md rounded-md flex flex-col justify-center items-center mb-16'>
+            <div className='bg-slate-300 w-11/12 py-12 h-fit drop-shadow-md rounded-md flex flex-col justify-center items-center mb-16'>
                 <div className='flex flex-col md:flex-row items-center gap-10 md:justify-evenly md:items-stretch w-full grow'>
-                    <div className='flex justify-center flex-col w-4/5 md:w-2/5'>
+                    <div className='flex justify-center flex-col w-11/12 md:w-2/5'>
                         <label className='py-2 text-xl'>Title</label>
                         <input className="p-2 mb-3 text-xl rounded-md" placeholder='Title' value={title} onChange={(e) => setTitle(e.target.value)} />
                         <label className='py-2 text-xl'>Description</label>
@@ -129,22 +130,22 @@ const RaiseFund = () => {
                         <label className='py-2 text-xl'>Target Amount</label>
                         <input className="p-2 mb-3 text-xl rounded-md" placeholder='Amount' value={amount} onChange={(e) => setAmount(e.target.value)} />
                     </div>
-                    <div className='relative flex justify-between items-center flex-col w-4/5 md:w-2/5 border-4 p-6 border-dashed border-slate-800 rounded-md h-96 md:h-auto'>
+                    <div className='relative flex justify-between items-center flex-col w-11/12 md:w-2/5 border-4 p-6 border-dashed border-slate-400 bg-blue-100 drop-shadow-xl rounded-md h-fit max-h-96 md:h-auto'>
                         {
                             image === "" ?
                                 <BsImage className='text-8xl fill-slate-500 grow' />
                                 :
                                 <img className='max-h-64' src={image} alt="Preview" />
                         }
-                        <label className='w-4/5 p-2 rounded-md text-xl text-center active:translate-y-1 bg-blue-500 text-neutral-100 hover:bg-blue-600'>
-                            <input type="file" className="absolute top-0 left-0  w-4/5 p-1 rounded hidden" onChange={(e) => handleImageSelect(e)} />
+                        <label className='w-4/5 p-2 mt-6 md:mt-0 rounded-md text-xl text-center active:translate-y-1 bg-blue-500 text-neutral-100 hover:bg-blue-600'>
+                            <input type="file" accept=".jpg,.jpeg,.png" className="absolute top-0 left-0  w-4/5 p-1 rounded hidden" onChange={(e) => handleImageSelect(e)} />
                             {
                                 image === "" ? "Upload Image" : "Change Image"
                             }
                         </label>
                     </div>
                 </div>
-                <button onClick={() => handleSubmit()} className='p-2 mt-16 w-96 rounded-md active:translate-y-1 bg-blue-500 text-neutral-100 hover:bg-blue-600 text-xl'>Submit Application</button>
+                <button onClick={() => handleSubmit()} className='p-2 mt-16 w-11/12 md:w-96 rounded-md active:translate-y-1 bg-blue-500 text-neutral-100 hover:bg-blue-600 text-xl'>Submit Application</button>
             </div>
         </div>
     )
